4d8773f485a0728132b074c15d56425f34ff2eb1
rob boll <robertcboll@gmail.com>
2015-06-02 23:41:14 -0400

    a lein project example

---
+++ sample-lein/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sample-lein
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ sample-lein/project.clj
@@ -0,0 +1,8 @@
+(defproject sample-lein "0.1.0-SNAPSHOT"
+  :description "a leiningen sample for vim-cp"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+                 
+                 ])
